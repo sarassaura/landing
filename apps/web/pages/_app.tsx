b400e970473fcbd36b36a69758eda37d242e5831
florescente <florescente@gmail.com>
2023-01-13 22:17:38 -0300

    build(app): add theme config

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,16 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
+import theme from "../styles/theme";
 import type { AppProps } from "next/app";
-
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
