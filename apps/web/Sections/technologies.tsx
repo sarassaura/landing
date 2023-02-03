7d72abd16135410d99d41bdee208ce1ae550dd34
florescente <florescente@gmail.com>
2023-02-03 11:28:13 -0300

    build(web): add transition animations

--- apps/web/Sections/technologies.tsx
+++ apps/web/Sections/technologies.tsx
@@ -1,14 +1,28 @@
 import { Container, colors, NextPage, useMediaQuery, Text } from 'ui'
+import { useRouter } from 'next/router'
+
 import Link from 'next/link'
 
 const Technologies = () => {
+  const router = useRouter()
   const [isNotMobile] = useMediaQuery('(min-width: 48em)')
+  const changePage = (up: number) => {
+    if (isNotMobile && up) {
+      if (up > 0) {
+        router.push('/#Services', undefined, { scroll: false })
+      }
+      if (up < 0) {
+        router.push('/#Contact', undefined, { scroll: false })
+      }
+    }
+  }
 
   return (
     <Container
       id="Technologies"
       direction="column"
       bgGradient={`linear(to-l,${colors.red.lightBackground}, ${colors.red.darkBackground})`}
+      changePage={changePage}
     >
       <Text>
         {isNotMobile ? 'Desktop Technologies' : 'Mobile Technologies'}
