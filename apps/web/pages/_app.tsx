1fde6be0d4abdb5fcc0aa79998b17655793b0895
florescente <florescente@gmail.com>
2023-02-03 12:06:42 -0300

    build(web): add navigation bar

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,41 @@
 import { AppProps } from 'next/app'
-import { ChakraProvider, theme, Layout, SideBar } from 'ui'
+import {
+  ChakraProvider,
+  theme,
+  Layout,
+  SideBar,
+  NavigationBar,
+  MenuItem,
+} from 'ui'
 import 'ui/styles/styles.css'
+import RightArrow from 'ui/assets/RightArrow'
+import Link from 'next/link'
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
   <ChakraProvider theme={theme}>
     <Layout>
+      <NavigationBar router={router.asPath}>
+        <Link href="#Home" scroll={false}>
+          <MenuItem router={router.asPath}>Home</MenuItem>
+        </Link>
+        <Link href="#Technologies" scroll={false}>
+          <MenuItem router={router.asPath}>About</MenuItem>
+        </Link>
+        <Link href="#Services" scroll={false}>
+          <MenuItem router={router.asPath}>Services</MenuItem>
+        </Link>
+        <Link href="#Contact" scroll={false}>
+          <MenuItem router={router.asPath}>
+            Contact &nbsp;
+            <RightArrow
+              width="12px"
+              height="7px"
+              marginBottom="2px"
+              color={router.asPath === '/#Contact' ? '#838383' : 'white'}
+            />
+          </MenuItem>
+        </Link>
+      </NavigationBar>
       <SideBar router={router.asPath} />
       <Component {...pageProps} />
     </Layout>
---
+++ packages/ui/NavigationBar.tsx
@@ -0,0 +1,30 @@
+import { Flex } from '@chakra-ui/react'
+import SmallLogo from './assets/SmallLogo'
+import { Menu } from './Menu'
+
+interface NavigationProps {
+  children: React.ReactNode
+  router: string
+}
+
+export const NavigationBar: React.FC<NavigationProps> = ({
+  children,
+  router,
+}) => (
+  <Flex
+    flexDirection="row"
+    position="fixed"
+    w="100%"
+    h="66px"
+    align="center"
+    borderBottom={
+      router === '/#Contact'
+        ? '1px solid rgba(83, 83, 83, 0.19)'
+        : '1px solid rgba(255, 255, 255, 0.19)'
+    }
+    display={['none', 'none', 'flex']}
+  >
+    <SmallLogo w={22} h={27} margin="0px 30px 0px 27px" />
+    <Menu>{children}</Menu>
+  </Flex>
+)
